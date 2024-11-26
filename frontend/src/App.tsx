856f70f5fb11bd5537a9dbb0158b7348fd50dac8
Danish-Belal <danishexplore019@gmail.com>
2024-11-27 01:28:26 +0530

    Chatbox init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,17 @@ import './App.css'
 function App() {
   const [socket, setSocket] = useState<WebSocket | null>(null);
   
-  const inputVal = useRef();
+  const inputVal = useRef("");
 
   function handleSubmit(){
     if(!socket){
+      console.log("No socket available");
+      
       return "No socket available";
     }
-    const message = inputVal.current.value;
+    const message = inputVal.current?.value;
+    console.log("Message in input box", message);
+    
 
     if(message === "ping"){
       console.log("Gonna Pinged msg", message);
@@ -42,8 +46,13 @@ function App() {
   return (
     <>
     <div>
-      <input type="text" ref={inputVal} placeholder='Message' />
-      <button onClick={handleSubmit}>Submit</button>
+      <div className='chat-box'>
+        <div className='msg-display'></div>
+        <div className='input-msg'>
+          <input className='input-text' type='text' placeholder='message...' ref={inputVal} />
+          <button className='submit-button' onClick={handleSubmit}>Submit</button>
+        </div>
+      </div>
     </div>
     </>
   )
